8f0f36c9d5305cf6ec9191b9339d97023b09be34
Swarnapudi Ishwar <swarnapudiishwar@gmail.com>
2026-01-13 05:04:18 +0530

    Remove keep-alive cron job - switch to on-demand Render instance

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const path = require('path');
 const compression = require('compression');
 const { warmChannelsList, startPeriodicWarming } = require('./utils/cacheWarmer');
-const { startKeepAlive, stopKeepAlive } = require('./utils/keepAlive');
 
 // Load project root .env first (useful when running from project root),
 // then load server/.env to allow overrides.
@@ -306,12 +305,6 @@ dbConnectionManager.connect(process.env.MONGO_URI, mongoOptions)
 				const localIP = getLocalIP();
 				console.log(`[DesiTV] Local:   http://localhost:${PORT}`);
 				console.log(`[DesiTV] Network: http://${localIP}:${PORT}`);
-			}
-			
-			// Start keep-alive for Render free tier (pings every 14 minutes)
-			const keepAliveInterval = startKeepAlive();
-			if (keepAliveInterval) {
-				console.log('[DesiTV] 🔄 Keep-alive service started');
 			}
 		});
 
